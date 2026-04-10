fae129d06a1d8756750ed121d5c5dab4c9553387
Astral Reverie <63588692+ai-1903@users.noreply.github.com>
2026-04-10 16:46:49 +0800

    Update ecosystem.config.js

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -2,20 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'Shiroi',
-      script: './server.js',
-      cwd: __dirname,
-      exec_mode: 'fork',
+      script: 'npx next start -p 2323',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '180M',
       env: {
         NODE_ENV: 'production',
-        HOSTNAME: '0.0.0.0',
-        PORT: process.env.PORT || 2323,
       },
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      merge_logs: true,
     },
   ],
 }
